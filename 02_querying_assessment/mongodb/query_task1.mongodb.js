1b681fe21308f60c26f029fbc11526c6b1971c5d
wiphada-sir <wiphada.sir@gmail.com>
2026-03-20 14:49:51 +0700

    added query_task1.mongodb.js

---
+++ 02_querying_assessment/mongodb/query_task1.mongodb.js
@@ -0,0 +1,27 @@
+// Task 1: Budget Meal Deal
+// The owner wants to introduce a budget-friendly meal deal promotion and needs to identify
+// which menu items could be included. To qualify, an item must be priced under $10.00,
+// so they can offer good value without cutting too deep into margins.
+//
+// Hint: Write a query to find all menu items in the menu_items collection that have a price less than 10.00.
+
+// Bonus: The dataset is identical in the PostgreSQL database, meaning the same business insight can be retrieved.
+// Write the equivalent query for PostgreSQL. See query_task1_bonus.sql
+
+// ---------------------------------------------------------------
+// Your thinking process (required)
+// ---------------------------------------------------------------
+// Before writing your query, explain in your own words how you
+// interpreted the task, what data you need, which collection(s)
+// are involved, and what MongoDB concepts you plan to use.
+// Write in English or Thai. Do not skip this step.
+//
+// Your thinking:
+// 1. use ("chrome-burger-db") database
+// 2. choose collection = menu_items
+// 3. use FIND to find the result
+// 4. choose price: and $lt to choose items that priced under $10
+use("chrome-burger-db");
+db.menu_items.find({
+    price: {$lt: 10}
+});
